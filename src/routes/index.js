27c6e1a2193acc32c2fdead74579e2ea441569b6
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2025-09-16 23:13:42 +0200

    Added log message for loading index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const wLogger = require('../util/logger');
 
 /* GET home page. */
 const model = { title: "My Sakila browser" };
 const view = 'index';
 router.get('/', function(req, res, next) {
+    wLogger.info("In index route");
   res.render(view, model);
 });
 
