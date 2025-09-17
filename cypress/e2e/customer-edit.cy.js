f4b1584ce2c32f28f8187311abb0e1b4ee0d76de
JarmoMarczinkowsky <jeh.marczinkowsky@student.avans.nl>
2025-09-17 15:41:36 +0200

    Added cypress to test application

---
+++ cypress/e2e/customer-edit.cy.js
@@ -0,0 +1,17 @@
+describe('Homepage functionality', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000')
+  })
+
+    it('Test', () => {
+      cy.get('#homepage-title').should('exist')
+    //   cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    //   cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    // });
+
+    })
+});
